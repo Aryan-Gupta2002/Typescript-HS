3466520a99fa9975fa3e1b0ab27d8ba8cd3b5245
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-08 19:18:27 +0530

    Interfaces

--- week-14-TS/src/index2.ts
+++ week-14-TS/src/index2.ts
@@ -1,10 +1,13 @@
-function greet(user:{
-    name:string,
-    age:number
-}){
+function greet(user:userType){
     console.log(`Hello ${user.name}`);
 }
+
 greet({
     name:"act",
     age:23
-})
+})// greet is an object passed to a function
+
+interface userType {
+    name:string,
+    age:number
+}

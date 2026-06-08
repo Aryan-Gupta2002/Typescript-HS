eeb4918044d0ab9bfc420b809d0d867261ad5951
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-08 17:40:58 +0530

    Type Script Programs

--- week-14-TS/index.js
+++ week-14-TS/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let x = 1; // Type Inferencing
+let x = 1; // Type Inferencing is implicit in JS
 let y = 1;
 // same as int x = 1, in C language
 console.log(x);
@@ -11,4 +11,21 @@ function greet(name) {
     console.log(`Hello ${name}`);
 }
 greet(name);
+// "any" is also kind of datatype other than number and string
+let num = 3;
+let num2 = 7;
+function add(n, n2) {
+    return num + num2;
+}
+console.log(add(num, num2));
+let age = 20;
+function check(age) {
+    if (age >= 18) {
+        console.log("Legal");
+    }
+    else {
+        console.log("Not legal");
+    }
+}
+check(age);
 //# sourceMappingURL=index.js.map
--- week-14-TS/index.ts
+++ week-14-TS/index.ts
@@ -1,4 +1,4 @@
-let x: number = 1; // Type Inferencing
+let x: number = 1; // Type Inferencing is implicit in JS
 let y: number|string = 1;
 // same as int x = 1, in C language
 console.log(x);
@@ -10,3 +10,23 @@ function greet(name : string){
     console.log(`Hello ${name}`);
 }
 greet(name);
+
+// "any" is also kind of datatype other than number and string
+
+let num :number = 3
+let num2 : number = 7
+function add(n:number,n2:number):number{
+    return num+num2;
+}
+console.log(add(num,num2)); 
+
+let age : number = 20;
+function check(age:number){
+    if (age >=18){
+        console.log("Legal");
+    }
+    else{
+        console.log("Not legal");
+    }
+}
+check(age);

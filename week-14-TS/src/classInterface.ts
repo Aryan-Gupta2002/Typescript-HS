bb193aeef690b7b4f98c9b248b15fe919eefabcd
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-08 21:39:58 +0530

    Interface Class Implementation

---
+++ week-14-TS/src/classInterface.ts
@@ -0,0 +1,35 @@
+
+interface People{
+    name:string,
+    age:number,
+    greet():string
+}
+
+let user:People={
+    name:"Oni",
+    age:31,
+    greet(){
+        return "Hello"
+    } // greet is a function
+}
+let greeting = user.greet();
+console.log(greeting);
+
+interface Person{
+    name:string,
+    age:number    
+}
+
+class Manager implements Person{
+    name: string;
+    age: number;
+    phn:number;
+    constructor(name:string,age:number,phn:number){
+        this.name = name;
+        this.age= age;
+        this.phn = phn;
+    }
+    
+}
+let newUser = new Manager("Aryan", 23, 22554);
+console.log(`${newUser.name}`);

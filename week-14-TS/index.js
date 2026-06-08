3ae61498743f50488aa11f22d603535b4a4fdedc
Aryan-Gupta2002 <ag912388@gmail.com>
2026-06-08 17:26:44 +0530

    Typescript program

---
+++ week-14-TS/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 1; // Type Inferencing
+let y = 1;
+// same as int x = 1, in C language
+console.log(x);
+// x = "text"; is not allowed
+console.log(x);
+let name = "Act";
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+greet(name);
+//# sourceMappingURL=index.js.map
--- week-14-TS/index.ts
+++ week-14-TS/index.ts
@@ -1,3 +1,12 @@
-let x: number = 1; // Type Inferencing 
+let x: number = 1; // Type Inferencing
+let y: number|string = 1;
 // same as int x = 1, in C language
 console.log(x);
+// x = "text"; is not allowed
+console.log(x);
+
+let name:string = "Act";
+function greet(name : string){
+    console.log(`Hello ${name}`);
+}
+greet(name);
